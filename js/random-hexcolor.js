98e5a651bbb31b7848b4aaa8ecefa8b820be7955
beginner-jk <valor9254@gmail.com>
2021-06-19 00:21:47 +0900

    Note the answer
    Note the answer

--- js/random-hexcolor.js
+++ js/random-hexcolor.js
@@ -2,25 +2,40 @@ const button = document.querySelector("button");
 const randomHexColor = document.querySelector("span");
 const body = document.querySelector("body")
 
-const arrNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const arrAlphabet = ["A", "B", "C", "D", "E", "F"];
+function hexGenerator() {
+    const hexNumbers = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F'
+];
 
-function ProduceRandomColor() {
-    let randomNumber = JSON.stringify(arrNumber[Math.floor(Math.random()*arrNumber.length)])+
-    JSON.stringify(arrNumber[Math.floor(Math.random()*arrNumber.length)])+
-    JSON.stringify(arrNumber[Math.floor(Math.random()*arrNumber.length)])+
-    JSON.stringify(arrNumber[Math.floor(Math.random()*arrNumber.length)])
+let result = "#";
 
-    let randomAlphabet = arrAlphabet[Math.floor(Math.random()*arrAlphabet.length)]+
-    arrAlphabet[Math.floor(Math.random()*arrAlphabet.length)];
+for (var i = 0; i < 6; i += 1) {
+    const randomIndex = Math.floor(Math.random() * hexNumbers.length);
+    result += hexNumbers[randomIndex];
+}
 
-    return "#"+randomNumber+randomAlphabet;
+return result;
 }
 
 function handlerColorChange(event) {
     event.preventDefault();
-    randomHexColor.innerText = ProduceRandomColor();
-    body.style.backgroundColor = ProduceRandomColor();
+    randomHexColor.innerText = hexGenerator();
+    body.style.backgroundColor = hexGenerator();
 }
 
 button.addEventListener("click", handlerColorChange);
